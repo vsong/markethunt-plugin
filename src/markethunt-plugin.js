a694ed3cc830f626f43abb3d8e9b5ac2731d4550
Victor Song <victor.xpsong@gmail.com>
2022-11-27 17:09:55 -0800

    update markethunt domain

--- src/markethunt-plugin.js
+++ src/markethunt-plugin.js
@@ -3,7 +3,7 @@
 // @author       Program
 // @namespace    https://greasyfork.org/en/users/886222-program
 // @license      MIT
-// @version      1.4.1
+// @version      1.4.2
 // @description  Adds a price chart and Markethunt integration to the MH marketplace screen.
 // @resource     jq_confirm_css https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css
 // @resource     jq_toast_css https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.css
@@ -17,7 +17,7 @@
 //
 // ==/UserScript==
 
-var markethuntDomain = 'markethunt.vsong.ca';
+var markethuntDomain = 'markethunt.win';
 var markethuntApiDomain = 'api.markethunt.win';
 
 MutationObserver =
